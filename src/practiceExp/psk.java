4139876ea745b2fc0096af1263cce6e693449142
SHANTHA KUMARI <SHANTHA KUMARI@DESKTOP-TJCOHS1>
2023-07-28 11:45:58 +0530

    Commit succesfully this data.

---
+++ src/practiceExp/psk.java
@@ -0,0 +1,20 @@
+package practiceExp;
+
+import org.testng.annotations.Test;
+
+public class psk {
+	
+	@Test
+	void test1() {
+		System.out.println("test1");
+	}
+
+	@Test
+	void test2() {
+		System.out.println("test2");
+	}
+	@Test
+	void test3() {
+		System.out.println("test3");
+	}
+}
